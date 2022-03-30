6183405ec1ddcc9bb97c871e46ba3a63bccfb3f8
Brisa-val <brisavaldebenito400@gmail.com>
2022-03-30 14:34:36 -0300

    Update testCafetera.php

--- testCafetera.php
+++ testCafetera.php
@@ -1,29 +1,30 @@
 <?php
 include "claseCafetera.php";
 
-$objCafetera = new cafetera(500,1000);
+$objCafetera = new Cafetera(400, 1000);
 echo $objCafetera."\n"."\n";
 $objCafetera->llenarCafetera();
 echo $objCafetera."\n"."\n";
-$comprobacion=$objCafetera->servirTaza(400);
+$comprobacion = $objCafetera->servirTaza(500);
 echo $comprobacion."\n";
 echo $objCafetera."\n"."\n";
-$comprobacion=$objCafetera->servirTaza(700);
+$comprobacion = $objCafetera->servirTaza(700);
 echo $comprobacion."\n";
 echo $objCafetera."\n"."\n";
 $operacionRealizada = $objCafetera->agregarCafe(500);
-if ($operacionRealizada){
+if($operacionRealizada){
     echo "se lleno la cafetera de forma exitosa"."\n"."\n";
 }else{
     echo "no se pudo llenar la cafetera ya que la cantidad que soporta es menor a la agregada"."\n"."\n";
 }
-$operacionRealizada=$objCafetera->agregarCafe(5000);
+$operacionRealizada = $objCafetera->agregarCafe(5000);
 if($operacionRealizada){
-    echo"se lleno la cafetera de forma exitosa"."\n"."\n";
+    echo "se lleno la cafetera de forma exitosa"."\n"."\n";
 }else{
-    echo" no se pudo llenar la cafetera ya que la cantidad que soporta es menor a la agregada"."\n"."\n";
+    echo "no se pudo llenar la cafetera ya que la cantidad que soporta es menor a la agregada"."\n"."\n";
 }
 echo $objCafetera."\n"."\n";
 $objCafetera->vaciarCafetera();
 echo $objCafetera."\n"."\n"
+
 ?>
